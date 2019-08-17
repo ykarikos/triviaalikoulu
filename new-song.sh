0de86f41991187147e0aca3a4886109b08ae70dc
Yrjö Kari-Koskinen <ykk@peruna.fi>
2019-08-17 22:39:14 +0300

    add script for creating a new song from a template

---
+++ new-song.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+TEMPLATE=$1
+SONG=$2
+
+if [ "$SONG" == "" ] || [ "$TEMPLATE" != "a4" ] && [ "$TEMPLATE" != "a8" ]; then
+	echo "Usage: $0 [a4|a8] \"Song Name\""
+	exit 0
+fi
+
+SONGFILE=`echo -n $SONG | tr [:upper:] [:lower:] | sed 's/ /-/g'`
+
+echo Copying template $TEMPLATE to songs/$SONGFILE...
+cp -r templates/$TEMPLATE songs/$SONGFILE
+cd songs/$SONGFILE
+rename "s/template\./$SONGFILE./" * parts/*
+find . -type f | xargs perl -pi -e "s/template\./$SONGFILE./"
+perl -pi -e "s/The Song Title/$SONG/" $SONGFILE.ly parts/$SONGFILE.header.ily
+
